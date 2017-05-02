eec4c2bcf32d13c9dd14303cdec440764be5b563
Bill <storywr@gmail.com>
2017-05-02 17:08:13 -0400

    added contact actions

---
+++ client/src/actions/contacts.js
@@ -0,0 +1,42 @@
+export const fetchContacts = () => {
+  return dispatch => {
+    return fetch('/api/contacts')
+      .then(response => response.json())
+      .then(contacts => dispatch({ type: 'LOAD_CONTACTS_SUCCESS', contacts }))
+      .catch(console.log)
+  }
+}
+
+export function addContact(contact) {
+  const request = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ contact })
+  };
+
+  return dispatch => {
+    return fetch('/api/contacts', request)
+      .then(response => response.json())
+      .then(contact => dispatch({ type: 'ADD_CONTACT', contact }))
+      .catch(console.log)
+  }
+}
+
+export function updateContact(contact) {
+  const request = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ contact })
+  };
+
+  return dispatch => {
+    return fetch('/api/contacts', request)
+      .then(response => response.json())
+      .then(contact => dispatch({ type: 'UPDATE_CONTACT', contact }))
+      .catch(console.log)
+  }
+}
