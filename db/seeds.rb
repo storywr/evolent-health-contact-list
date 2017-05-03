b66aacc828495874f5b4d8feeeea3a189870c2fe
Bill <storywr@gmail.com>
2017-05-02 21:43:19 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-Contact.create({first_name: "Tom", last_name: "Petty", email: "tompetty@gmail.com", phone_number: "123-456-7890", status: true})
-Contact.create({first_name: "Steve", last_name: "Miller", email: "stevemiller@gmail.com", phone_number: "111-111-1111", status: true})
-Contact.create({first_name: "Bruce", last_name: "Springsteen", email: "brucespringsteen@gmail.com", phone_number: "222-222-2222", status: false})
+Contact.create({first_name: "Tom", last_name: "Petty", email: "tpetty@gmail.com", phone_number: "111-111-1111", status: true})
+Contact.create({first_name: "Steve", last_name: "Miller", email: "smiller@gmail.com", phone_number: "222-222-2222", status: true})
+Contact.create({first_name: "Bruce", last_name: "Springsteen", email: "bspringsteen@gmail.com", phone_number: "333-333-3333", status: false})
+Contact.create({first_name: "Joe", last_name: "Walsh", email: "jwalsh@gmail.com", phone_number: "444-444-4444", status: false})
+Contact.create({first_name: "Eric", last_name: "Clapton", email: "eclapton@gmail.com", phone_number: "555-555-5555", status: false})
